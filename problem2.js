08c129319588bffb3484e337ebf20a5e9a10495a
unknown <khatikshubham1995@gmail.com>
2023-08-23 09:01:58 +0530

    contains duplicate easy

---
+++ problem2.js
@@ -0,0 +1,29 @@
+/*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true*/
+
+const containsDuplicate = function (nums) {
+    // Create an empty Map to store elements as keys and their presence as values
+    const map = new Map();
+    for (const n of nums) {
+      if (map.has(n)) {
+        return true;
+      }  
+      map.set(n, true);
+    }
+    return false;
+  };
+  let nums = [7, 5, 8, 6, 3, 7];
+  const result = containsDuplicate(nums);
+  console.log(result);
+
+  
+   // Iterate through each element 'n' in the 'nums' array
+  // Check if the current element 'n' already exists in the map
+   // If it exists, there's a duplicate, so return true
+  // If the element 'n' doesn't exist in the map, add it with value 'true'
+  // If no duplicates were found, return false
+// Define an array of numbers
+// Call the 'containsDuplicate' function with the 'nums' array
+// Print the result to the console
